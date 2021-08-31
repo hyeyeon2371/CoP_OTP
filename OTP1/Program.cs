e6346f9868e6f93e8a949d7a68eb675f817aad95
hyeyeon2371 <gpdus2371@gmail.com>
2021-08-31 17:11:40 +0900

    WPF 앱 생성 및 컴포넌트 배치

--- OTP1/Program.cs
+++ OTP1/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 
+
 namespace OTP1
-{
+{ 
     public class TOTP
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+        public static void Test()
+        {
+
         }
     }
 }
