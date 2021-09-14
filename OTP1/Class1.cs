3e1f162fd960cbe4784af43ca1766a776c6b3fbe
hyeyeon2371 <gpdus2371@gmail.com>
2021-09-14 11:17:44 +0900

    TOTP 인증 함수 구현

--- OTP1/Class1.cs
+++ OTP1/Class1.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Security.Cryptography;
-using System.Collections;
+using System.Linq;
 
 // Hyeyeon Park 
 namespace OTP1
@@ -8,7 +8,7 @@ namespace OTP1
     public class TOTP
     {
 
-        public static String GenerateOTP(String secretKey, int seconds, int digit)
+        public static String GenerateOTP(String secretKey, int seconds, int digit, String algorithm)
         {
             DateTime startTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
             long counter = (long)Math.Floor((DateTime.UtcNow - startTime).TotalSeconds / seconds);
@@ -18,9 +18,25 @@ namespace OTP1
             Array.Reverse(message);
             byte[] secret = System.Text.Encoding.UTF8.GetBytes(secretKey);
             byte[] hash;
-            using (HMACSHA1 hmac = new HMACSHA1(secret, true))
+
+
+            switch (algorithm)
             {
-                hash = hmac.ComputeHash(message);
+                case "SHA1":
+                    using (HMACSHA1 hmac = new HMACSHA1(secret, true))
+                    {
+                        hash = hmac.ComputeHash(message);
+                    }
+                    break;
+
+                case "SHA256":
+                default:
+                    using (HMACSHA256 hmac = new HMACSHA256(secret))
+                    {
+                        hash = hmac.ComputeHash(message);
+                    }
+                    break;
+         
             }
 
             int binaryCode = GetBinayCode(hash);
@@ -52,6 +68,63 @@ namespace OTP1
                 ((hash[offset + 2] & 0xff) << 8) |
                 (hash[offset + 3] & 0xff);
             return binaryCode;
+        }
+
+        static int idx = 0;
+        static String[] prevOtp = new String[3];
+        public static Boolean VerifyOTP(String otp, String secretKey, int seconds, int digit, String algorithm)
+        {
+            // Log
+            Console.WriteLine("1) Start Verify");
+            printOtpLog();
+
+
+            // 이미 사용된 번호 검증 
+            foreach (var item in prevOtp)
+            {
+                if (item != null && item.Equals(otp))
+                {
+                    return false;
+                }
+            }
+
+            // 검증된 번호는 Array에 보관 
+            String currentOtp = GenerateOTP(secretKey, seconds, digit, algorithm);
+            if (otp.Equals(currentOtp.Replace(" ", "")))
+            {
+                if(idx == prevOtp.Length)
+                {
+                    idx = 0;
+                }
+
+                prevOtp[idx++] = otp;
+               
+                // Log
+                Console.WriteLine("2) End Verify");
+                printOtpLog();
+
+                return true;
+            }
+
+            return false;
+        }
+
+        private static void printOtpLog()
+        {
+           var  msg = "";
+            foreach (var item in prevOtp)
+            {
+                if (item == null)
+                {
+                    msg += "EMPTY  ";
+                }
+                else
+                {
+                    msg += item + "  ";
+                }
+
+            }
+            Console.WriteLine(msg);
         }
     }
 }
